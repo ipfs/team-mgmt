b84680f4dbf57e459828d028a2d282373beca3a9
ᴠɪᴄᴛᴏʀ ʙᴊᴇʟᴋʜᴏʟᴍ <victorbjelkholm@gmail.com>
2017-11-08 07:15:21 -0500

    Add script for syncing labels across repos (#508)

---
+++ tools/sync-labels/generate-repository-list.js
@@ -0,0 +1,66 @@
+/* Generates a list of repositories and puts it in .config.json
+ *
+ *  GITHUB_AUTH_TOKEN environment variable needs to be set to run this command
+ *
+ */
+
+if (!process.env.GITHUB_AUTH_TOKEN) {
+  console.log('Error: GITHUB_AUTH_TOKEN environment variable needs to be set to run this command')
+  process.exit(1)
+}
+const GH_TOKEN = process.env.GITHUB_AUTH_TOKEN
+
+const fetch = require('node-fetch')
+const fs = require('fs')
+const per_page = 100
+const organizations = [
+  'ipfs',
+  'libp2p',
+  'ipld',
+  'multiformats',
+  'ipfs-shipyard'
+]
+
+const fetchResults = (org, total = [], page = 1) => new Promise((resolve, reject) => {
+  console.log(`total fetched so far for ${org}`, total.length)
+  console.log('on page', page)
+  fetch(`https://api.github.com/orgs/${org}/repos?per_page=${per_page}&page=${page}`, {
+    headers: {
+      "Authorization": `Token ${GH_TOKEN}`
+    }
+  })
+    .then(res => res.json())
+    .then((res) => {
+      total = total.concat(res)
+      console.log('fetched', res.length)
+      if (res.length !== per_page) {
+        resolve(total)
+      } else {
+        fetchResults(org, total, page + 1).then(resolve)
+      }
+    }).catch(reject)
+})
+
+let current = 0
+let allRepos = []
+
+const done = () => {
+  console.log('Total number of repositories', allRepos.length)
+  const config = require('./config.json')
+  config[0].repositories = allRepos.map(r => r.full_name), null, 2
+  fs.writeFileSync(
+    './config.json',
+    JSON.stringify(config, null, 2)
+  )
+}
+
+organizations.forEach((org) => {
+  fetchResults(org).then((res) => {
+    console.log(`total received from ${org}`, res.length)
+    allRepos = allRepos.concat(res)
+    current = current + 1
+    if (current === organizations.length) {
+      done()
+    }
+  })
+})
---
+++ tools/sync-labels/readme.md
@@ -0,0 +1,14 @@
+## Tool for syncing labels
+
+Syncs:
+
+- libp2p
+- ipld
+- ipfs
+- multiformats
+- ipfs-shipyard
+
+# Usage
+
+- `npm install`
+- `GITHUB_AUTH_TOKEN=changeme npm start`
